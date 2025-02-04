97ab03a26ec3f857737bbd26d0d70c72f202731d
Vikash <kr.vikash3491@gmail.com>
2025-02-04 11:37:16 +0530

    Initial commit

---
+++ src/main/java/com/vikash/jooq/SpringJooqBasicApplication.java
@@ -0,0 +1,13 @@
+package com.vikash.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJooqBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikash/jooq/SpringJooqBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikash.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJooqBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
